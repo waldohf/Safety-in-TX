e203cbc05c55baf4622d989ee449997a61abf376
waldohf <waldohf@gmail.com>
2013-12-09 01:22:00 -0600

    Update twitter_dump2.py

--- twitter_dump2.py
+++ twitter_dump2.py
@@ -4,6 +4,7 @@
 
 import tweepy
 import time
+import re
 import math
 import sys
 from random import randint
@@ -55,34 +56,41 @@ class TwitterCrawler():
     def crawl_user_tweets(self, user_id, the_count, total_tweets, attempts, since):
         self.check_api_rate_limit(900)
         try:
-                tweets = self.api.user_timeline(user_id, count = the_count, since_id = since)
+                tweets = self.api.user_timeline(user_id, count = the_count)
+                # tweets = self.api.user_timeline(user_id, count = the_count, since_id = since)
                 # tweets = self.api.user_timeline(user_id, count = the_count, since_id = since, exclude_replies = 'true', include_rts = 'false')
         except:
             tweets = None
 
+        if (len(tweets) < 1):
+            return []
         tried_count = 0
         temp_max = [0 for x in range(3)]
-        temp_max.append(tweets[0]['id'])
+        temp_max.append(tweets[len(tweets)-1]['id'])
+        newest = tweets[0]['id']
         while len(tweets) < total_tweets:
             try:
                 temp_max[0:1] = []
                 temp_max.append(temp_max[2])
-                for tweet in tweets:
-                    if (tweet['id'] > temp_max[3]):
+                for idx, tweet in enumerate(tweets):
+                    if (tweet['id'] == since):
+                         return (tweets[:idx], temp_max[3], newest)
+                    if (temp_max[3] > tweet['id']):
                         temp_max[3] = tweet['id']
 
                 print temp_max[3]
                 if (temp_max[0] == temp_max[3]):
-                    return (tweets, temp_max[3])
+                    return (tweets, temp_max[3], newest)
 
-                tweets.extend(self.api.user_timeline(user_id, count = the_count, since_id = temp_max[3]))
+                tweets.extend(self.api.user_timeline(user_id, count = the_count, max_id = temp_max[3]-1))
+                # tweets.extend(self.api.user_timeline(user_id, count = the_count, since_id = since))
                 # tweets.extend(self.api.user_timeline(user_id, count = the_count, since_id = temp_max[3], exclude_replies = 'true', include_rts = 'false'))
             except:
                 pass
             tried_count += 1
             if tried_count == attempts:
                 break
-        return (tweets, temp_max[3])
+        return (tweets, temp_max[3], newest)
 
 def main():
     tc = TwitterCrawler()
@@ -90,7 +98,16 @@ def main():
     filenames = ['dallas_tweets.txt', 'fort_worth_tweets.txt', 'houston_tweets.txt', 'austin_tweets.txt', 'san_antonio_tweets.txt', 'el_paso_tweets.txt']
 
     files = []
+    since_ids = []
     for filename in filenames:
+        temp_file = open(filename, 'r')
+        for line in temp_file:
+            if '--**--**--' in line:
+                ln = line[11:]
+                idx = ln.find(',')
+                idx2 = ln[idx:].find('--**--**--') - 1
+                since_ids.append(ln[:idx] + ' ' + ln[idx+13:idx2])
+        temp_file.close()
         files.append(open(filename, 'a'))
 
     # Dallas (index 0)
@@ -132,9 +149,15 @@ def main():
     # @ElPaso__News 15,238
     # @NC9 12,602
 
-    # the format is: twitter ID or screen name, which file index to write to, 1, then 0
-    tweeters = [['DallasNews', 0, 1, 0], ['FtWorthNews', 1, 1, 0], ['abc13houston', 2, 1, 0], ['foxaustin', 3, 1, 0], ['ksatnews', 4, 1, 0], ['elpasotimes', 5, 1, 0], ['dallasnews_com', 0, 1, 0], ['News_FortWorth', 1, 1, 0], ['MyFoxHouston', 2, 1, 0], ['KVUE', 3, 1, 0], ['News4SA', 4, 1, 0], ['NewsElPasoTX', 5, 1, 0], ['TotalTrafficDFW', 0, 1, 0], ['News_Fort_Worth', 1, 1, 0], ['HoustonTraffic', 2, 1, 0], ['YNNAustin', 3, 1, 0], ['KENS5', 4, 1, 0], ['ElPaso__News', 5, 1, 0], ['SDaviesNBC5', 1, 0, 0], ['News_Houston_TX', 2, 0, 0], ['HoustonChron', 2, 0, 0], ['KHOU', 2, 0, 0], ['KXAN_News', 3, 0, 0], ['TotalTrafficSAT', 4, 0, 0], ['mySA', 4, 0, 0], ['NC9', 5, 0, 0]]
+    # the format is: twitter ID or screen name, which file index to write to, since id, then 0
+    tweeters = [['DallasNews', 0, 0, 0], ['FtWorthNews', 1, 0, 0], ['abc13houston', 2, 0, 0], ['foxaustin', 3, 0, 0], ['ksatnews', 4, 0, 0], ['elpasotimes', 5, 0, 0], ['dallasnews_com', 0, 0, 0], ['News_FortWorth', 1, 0, 0], ['MyFoxHouston', 2, 0, 0], ['KVUE', 3, 0, 0], ['News4SA', 4, 0, 0], ['NewsElPasoTX', 5, 0, 0], ['TotalTrafficDFW', 0, 0, 0], ['News_Fort_Worth', 1, 0, 0], ['HoustonTraffic', 2, 0, 0], ['YNNAustin', 3, 0, 0], ['KENS5', 4, 0, 0], ['ElPaso__News', 5, 0, 0], ['SDaviesNBC5', 1, 0, 0], ['News_Houston_TX', 2, 0, 0], ['HoustonChron', 2, 0, 0], ['KHOU', 2, 0, 0], ['KXAN_News', 3, 0, 0], ['TotalTrafficSAT', 4, 0, 0], ['mySA', 4, 0, 0], ['NC9', 5, 0, 0]]
+    for s in since_ids:
+        pair = s.split()
+        for i, tweeter in enumerate(tweeters):
+            if (pair[0] == tweeter[0]):
+                tweeters[i][2] = pair[1]
 
+    print tweeters
     # flags to tell whether all tweets were pulled from each individual tweeter
     complete = [0 for x in range(len(tweeters)+1)]
     last = len(complete)-1
@@ -146,32 +169,45 @@ def main():
             print user['screen_name'] + ' has ' + str(user['statuses_count']) + ' tweets.'
 
             tweets = tc.crawl_user_tweets(tweeter[0], 800, (user['statuses_count']-tweeter[3]), int(math.ceil(user['statuses_count']/200.0)), tweeter[2])
+            if (len(tweets) == 0):
+                continue
             print 'retreived ' + str(len(tweets[0])) + ' of those tweets.\n'
             # update that tweeter's total tweets pulled
             tweeters[index][3] += len(tweets[0])
             # update that tweeter's since tweet ID
             tweeters[index][2] = tweets[1]
-            if (tweeters[index][3] > 3200):
-                complete[index] = 1
+            # if (tweeters[index][3] > 3200):
+                # complete[index] = 1
 
             for tweet in tweets[0]:
                 temp = tweet['text'].encode('utf-8')
                 temp = (temp.lower()).strip(' \t\n\r')
                 temp = (temp.replace('\n',' ')).replace('\r',' ')
                 temp = (temp.replace('\'',' ')).replace('\"',' ')
-                temp = (temp.replace('-',' ')).replace('!',' ')
+                temp = (temp.replace('\t',' ')).replace('-',' ')
                 temp = (temp.replace('[',' ')).replace(']',' ')
-                this_doc = temp.replace('?',' ')
+                temp = (temp.replace('ó','o')).replace('í','i')
+                temp = (temp.replace('é','e')).replace('ñ','n')
+                temp = (temp.replace('á','a')).replace('â','a')
+                temp = (temp.replace('Á','a')).replace('à','a')
+                temp = (temp.replace('ç','c')).replace('¡','i')
+                temp = (temp.replace('ë','e')).replace('è','e')
+                temp = temp.replace('ú','u')
+                pat = re.compile('(…|°|“|”|’|′|‘|″|`|´|–|½| |😱|😶|☺️|😘|❤️|😊|✈️|©|😏|☁️|⚡️|☔️|♥|®|😁|😄|•|—|º|™|▸|😆|¿|›|❄|▪|�|😋||¢|😜|◆|😎|😞|😢|😔|😂|★|☆)')
+                temp = pat.sub('', temp)
+                this_doc = (temp.replace('?',' ')).replace('!',' ')
                 temp = str(tweet['created_at']).replace('+0000 ', '')
                 files[tweeter[1]].write(temp + ' ' + this_doc + '\n')
             files[tweeter[1]].write('--**--**-- ' + tweeter[0] + ', since ID = ' + str(tweets[2]) + ' --**--**--\n')
 
         complete[last] = 1
+        '''
         for idx in range(last):
             if (complete[idx] == 0):
                 complete[last] = 0
                 break
         time.sleep(3600)
+        '''
 
     for f in files:
         f.close()
